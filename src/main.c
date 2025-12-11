196efd88536952f6e6d02034a4814c43eb083698
hyperioxx <hyperioxx>
2025-12-11 19:37:47 +0000

    unused variables

--- src/main.c
+++ src/main.c
@@ -1,9 +1,5 @@
 #include "uart.h"
 
-extern unsigned long _bss_start;
-extern unsigned long _bss_end;
-
-volatile unsigned int should_be_zero;
 
 int kernel_main(void) {
     uart_print("hello, world\n");
