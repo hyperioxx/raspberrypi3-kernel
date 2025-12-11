bd62de8d36f1f0f6d7deeb6af3ea371babdcfa5b
hyperioxx <hyperioxx>
2025-12-11 19:36:30 +0000

    remove check if bss space has been cleared at boot

--- src/main.c
+++ src/main.c
@@ -6,13 +6,6 @@ extern unsigned long _bss_end;
 volatile unsigned int should_be_zero;
 
 int kernel_main(void) {
-    // we expect this to be 0 if .bss was cleared
-    // just a simple check using uart_print for now
-    if (should_be_zero == 0) {
-        uart_print("BSS OK: should_be_zero == 0\n");
-    } else {
-        uart_print("BSS BROKEN: should_be_zero != 0\n");
-    }
     uart_print("hello, world\n");
     while (1) {
     }
